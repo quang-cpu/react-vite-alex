433a9eeee5cfbd7b53ff5defa30d6a54fabf3579
quang-cpu <madeinthuandao2018@gmail.com>
2024-08-16 13:45:25 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,15 @@
+//JSX : 1 parent
+//fragment
+import './style.css'
+
 const MyComponent = () => {
     return (
-        <div> alex & frontend update </div>
+        <>
+            <div> alex & frontend update </div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >child</div>
+        </>
     );
 }
 
