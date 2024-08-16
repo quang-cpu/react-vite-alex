618e0ec60a46a6c07caa9b579852397ac123b269
quang-cpu <madeinthuandao2018@gmail.com>
2024-08-16 09:07:05 +0700

    video 29

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,7 @@
+const MyComponent = () => {
+    return (
+        <div> alex & frontend update </div>
+    );
+}
+
+export default MyComponent
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,17 @@
+const ThirdComponent = () => {
+    return (
+        <div>
+            Third component
+        </div>
+    )
+}
+
+const SecondComponent = () => {
+    return (
+        <div>
+            Second component
+        </div>
+    )
+}
+
+export { SecondComponent, ThirdComponent };
